01909ff5142d7f49e584c45af1c22af11657454f
wilson rodrigues <wilsonrodriguesbraga@gmail.com>
2019-11-28 20:14:09 -0300

    criado user service

---
+++ src/main/java/com/wallet/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.wallet.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+import com.wallet.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public User save(User user) {
+		return repository.save(user);
+	}
+
+	@Override
+	public Optional<User> findByEmail(String email) {
+		return repository.findByEmailEquals(email);
+	}
+	
+	
+	
+}
